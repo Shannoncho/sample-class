5edd42f03af734760c23134efa7c653a56b45009
Shannoncho <170471@hkis.edu.hk>
2016-09-01 09:54:13 +0800

    init

---
+++ Chocolate.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Chocolate here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Chocolate
+{
+ 
+     public static void main(String args[])
+      {
+          Chocolate = new Chocolate();
+          System.out.println("Chocolate");
+          
+}
---
+++ Chocolatetest.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Chocolatetest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Chocolatetest
+{
+    // instance variables - replace the example below with your own
+      private String Chocolate;
+      private int weight;
+      private String looks;
+      private String Color;
+     public Chocolate()
+     {
+            Looks = "round";
+            Weight = 1;
+            Color = "black";
+    }
+}
